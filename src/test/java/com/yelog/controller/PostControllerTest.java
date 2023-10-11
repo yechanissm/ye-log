764c035494358dcccb26eab8223b88b337c0f989
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-10-12 02:17:55 +0900

    컨트롤러 생성

---
+++ src/test/java/com/yelog/controller/PostControllerTest.java
@@ -0,0 +1,30 @@
+package com.yelog.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+class PostControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void test() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/posts"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+}
