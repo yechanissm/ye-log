1cf92baa89c5b74fe916db5890b876747f647eab
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-10-12 02:04:05 +0900

    프로젝트 생성

---
+++ src/main/java/com/yelog/YelogApplication.java
@@ -0,0 +1,13 @@
+package com.yelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YelogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yelog/YelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YelogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
