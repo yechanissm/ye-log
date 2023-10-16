e27339b0ba9b011677619ba79693c60d99372072
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-10-17 01:52:26 +0900

    시큐리티 기본 설정

---
+++ src/main/java/com/yelog/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.yelog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers("/favicon.ico","/error")
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests()
+                    .requestMatchers("/auth/login").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
+}
--- src/main/java/com/yelog/controller/AuthController.java
+++ src/main/java/com/yelog/controller/AuthController.java
@@ -8,6 +8,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,11 @@ public class AuthController {
 
     private final AppConfig appConfig;
     private final AuthService authService;
+
+    @GetMapping("/auth/login")
+    public String login() {
+        return "로그인 페이지입니다.";
+    }
 
 
     @PostMapping("/auth/signup")
